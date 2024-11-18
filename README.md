f1a99a973391d8317106edfc43288fa833f8c7f9
forthang <therealskyvvalker@gmail.com>
2024-11-18 10:09:44 +0300

    18.11: map init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MISSIS_Map
 map of MISSIS campuses 
+
+OSM - https://www.openstreetmap.org/node/1012852234
+
+OSM documentation - https://wiki.openstreetmap.org/wiki/API_v0.6
+
+Additional lib(just check) - https://habr.com/ru/companies/vk/articles/591879/ 
+
+logging documentation - https://docs.python.org/3/library/logging.html
+
+
--- server/main.py
+++ server/main.py
@@ -1,11 +1,31 @@
-from fastapi import FastAPI
+import folium
+import requests
+import logging
+import xml.etree.ElementTree as ET
 
-app = FastAPI()
+API = 'https://api.openstreetmap.org/'
+data = '15,15,15,15'
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+#get xml from api
+response = requests.get(API + 'api/0.6/map?bbox=' + data)
 
+#xml parse
+root = ET.fromstring(response.content)
 
-#develop branch
-# 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
+#create map
+m = folium.Map(location=[55.7268349, 37.6059714], zoom_start=25)
+
+#parse nodes
+#lat - latitude, lon-longitude
+for node in root.findall('node'):
+    lat = float(node.get('lat'))
+    lon = float(node.get('lon'))
+    folium.Marker(location=[lat, lon]).add_to(m)
+
+#to html
+m.save("map.html")
+
+#logging
+logging.basicConfig(level=logging.INFO, filename="MISSIS_Map_logs.log",filemode="w")
+logging.info('Map has been saved to map.html')
+
