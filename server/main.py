72e396717d13bbc39d9b24fa2f4b8be9be447180
forthang <therealskyvvalker@gmail.com>
2024-11-18 19:07:20 +0300

    add parse XML

--- server/main.py
+++ server/main.py
@@ -4,28 +4,35 @@ import logging
 import xml.etree.ElementTree as ET
 
 API = 'https://api.openstreetmap.org/'
-data = '15,15,15,15'
+data = '37.6173,55.7268,37.6183,55.7273'  # Bounding box
 
-#get xml from api
+# Get XML from API
 response = requests.get(API + 'api/0.6/map?bbox=' + data)
 
-#xml parse
+# XML parse
 root = ET.fromstring(response.content)
 
-#create map
-m = folium.Map(location=[55.7268349, 37.6059714], zoom_start=25)
+# Create map
+m = folium.Map(location=[55.7268349, 37.6173], zoom_start=25)
 
-#parse nodes
-#lat - latitude, lon-longitude
+# Parse nodes
+#/brief take points like (x,y) for creating rectangles or marking location
 for node in root.findall('node'):
     lat = float(node.get('lat'))
     lon = float(node.get('lon'))
-    folium.Marker(location=[lat, lon]).add_to(m)
+    pass
 
-#to html
+# Add a marker for the specified location
+folium.Marker(
+    location=[55.7268349, 37.6173],
+    popup='Ленинский проспект, 6',
+    icon=folium.Icon(color='red')
+).add_to(m)
+
+# Save to HTML
 m.save("map.html")
 
-#logging
-logging.basicConfig(level=logging.INFO, filename="MISSIS_Map_logs.log",filemode="w")
+# Logging
+logging.basicConfig(level=logging.INFO, filename="MISSIS_Map_logs.log", filemode="w")
 logging.info('Map has been saved to map.html')
-
+logging.info('Response content:\n' + response.text)
