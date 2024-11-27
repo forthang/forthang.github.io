c9a1cbc8fb875aef35bf72d0f60752b7736a8fa6
root <root@WIN-6BB1N3RJ5AD.>
2024-11-28 01:03:17 +0300

    add map to react render

---
+++ server/features.py
@@ -0,0 +1,7 @@
+import sys
+
+def side_panel():
+    return 0
+
+
+sys.modules[__name__] = sidePanel
--- server/main.py
+++ server/main.py
@@ -1,38 +1,20 @@
 import folium
-import requests
 import logging
-import xml.etree.ElementTree as ET
-
-API = 'https://api.openstreetmap.org/'
-data = '37.6173,55.7268,37.6183,55.7273'  # Bounding box
-
-# Get XML from API
-response = requests.get(API + 'api/0.6/map?bbox=' + data)
-
-# XML parse
-root = ET.fromstring(response.content)
+from blockA import block_a
 
 # Create map
-m = folium.Map(location=[55.7268349, 37.6173], zoom_start=25)
+m = folium.Map(location=[55.7282903, 37.6096873], zoom_start=15)
 
 # Parse nodes
 #/brief take points like (x,y) for creating rectangles or marking location
-for node in root.findall('node'):
-    lat = float(node.get('lat'))
-    lon = float(node.get('lon'))
-    pass
 
-# Add a marker for the specified location
-folium.Marker(
-    location=[55.7268349, 37.6173],
-    popup='Ленинский проспект, 6',
-    icon=folium.Icon(color='red')
-).add_to(m)
+block_a(m)
 
 # Save to HTML
-m.save("map.html")
+m.save("../client/public/map.html")
 
 # Logging
 logging.basicConfig(level=logging.INFO, filename="MISSIS_Map_logs.log", filemode="w")
 logging.info('Map has been saved to map.html')
-logging.info('Response content:\n' + response.text)
+# logging.info('Response content:\n' + response.text)
+
