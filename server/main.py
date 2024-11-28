26d22c380223d32820a647cb3a4d766abe6ab54d
Ekaterina-Mordasova26 <katy-dm@mail.ru>
2024-11-28 22:12:21 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 import folium
 import logging
 from blockA import block_a
+from coordinates import corp
+from coordinates_dormitories import dormitories
 
 # Create map
 m = folium.Map(location=[55.7282903, 37.6096873], zoom_start=15)
@@ -8,7 +10,11 @@ m = folium.Map(location=[55.7282903, 37.6096873], zoom_start=15)
 # Parse nodes
 #/brief take points like (x,y) for creating rectangles or marking location
 
-block_a(m)
+for i in range(len(corp["building"]["loc"])):
+    block_a(m, corp["building"]["loc"][i])
+    
+for j in range(len(dormitories["building"]["loc"])):
+    block_a(m, dormitories["building"]["loc"][j])
 
 # Save to HTML
 m.save("../client/public/map.html")
@@ -17,4 +23,3 @@ m.save("../client/public/map.html")
 logging.basicConfig(level=logging.INFO, filename="MISSIS_Map_logs.log", filemode="w")
 logging.info('Map has been saved to map.html')
 # logging.info('Response content:\n' + response.text)
-
