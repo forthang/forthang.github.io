74fe57cfa1def81968a29d370c9a91569eb8cdde
Ekaterina-Mordasova26 <katy-dm@mail.ru>
2024-12-03 03:26:10 +0300

    Update blockA.py

--- server/blockA.py
+++ server/blockA.py
@@ -1,4 +1,5 @@
 import folium
+import branca
 from coordinates_corp import corp
 from coordinates_dormitories import dormitories
 
@@ -8,6 +9,15 @@ def block_a(m):
     for i in range (len(corp["building"])):
         locat = corp["building"][i]["loc"]
         name = corp["building"][i]["name"]
+        html = f"""
+            <body style=background-color:PaleTurquoise>
+            <h1> <font face="impact" size="7" color="DarkBlue"> {corp["building"][i]["name"]} </font></h1>
+            <p><font face="Arial" size="3"> {corp["building"][i]["address"]} </font></p>
+            <p><font face="Arial" size="3"> {corp["building"][i]["info"]} </font></p>
+            <img src={corp["building"][i]["img"]} width=490 height=280>
+        """
+        iframe = branca.element.IFrame(html=html, width=510, height=500)
+        popup = folium.Popup(iframe, max_width=500)
         folium.Polygon(
             locations=locat,
             color=color,
@@ -15,6 +25,7 @@ def block_a(m):
             fill_color= color,
             fill_opacity=0.3,
             fill=True,
+            popup=popup,
             tooltip=name).add_to(m)
 
 def block_b(m):
@@ -23,6 +34,14 @@ def block_b(m):
     for j in range (len(dormitories["building"])):
         locat = dormitories["building"][j]["loc"]
         name = dormitories["building"][j]["name"]
+        html = f"""
+            <h1> <font face="impact" size="7" color="gray"> {dormitories["building"][j]["name"]} </font></h1>
+            <p><font face="Arial" size="3"> {dormitories["building"][j]["address"]} </font></p>
+            <p><font face="Arial" size="3"> {corp["building"][j]["info"]} </font></p>
+            <img src={dormitories["building"][j]["img"]} width=300 height=280>
+        """
+        iframe = branca.element.IFrame(html=html, width=510, height=500)
+        popup = folium.Popup(iframe, max_width=500)
         folium.Polygon(
             locations=locat,
             color=color,
@@ -30,5 +49,6 @@ def block_b(m):
             fill_color= color,
             fill_opacity=0.3,
             fill=True,
+            popup=popup,
             tooltip=name).add_to(m)
     
